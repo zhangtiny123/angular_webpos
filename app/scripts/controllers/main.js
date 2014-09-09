eba8950d365c06197730985848d4a8eaa928c8db
tiny <tzhang920314@gmail.com>
2014-09-09 09:17:43 +0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularWebposApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularWebposApp
+ */
+angular.module('angularWebposApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
